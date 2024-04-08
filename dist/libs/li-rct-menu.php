054f3c2ddc3fbf45c578c754db0a63a14e52b852
dillact-tera <tera@dillact.com>
2024-04-08 11:29:39 +0900

    メニューリンク修正

--- dist/libs/li-rct-menu.php
+++ dist/libs/li-rct-menu.php
@@ -1,6 +1,6 @@
 <ul class="p-rct-menu scroll-anim to-anim-top">
     <li><a href="<?php echo APP_URL; ?>new-graduate/" class="opacity"><span>新卒採用</span></a></li>
-    <li><a href="<?php echo APP_URL; ?>mid-career/#reception" class="opacity"><span>レセプション<br>採用</span></a></li>
-    <li><a href="<?php echo APP_URL; ?>mid-career/#backoffice" class="opacity"><span>バックオフィス<br>採用</span></a></li>
+    <li><a href="<?php echo APP_URL; ?>mid-career/?category=reception" class="opacity"><span>レセプション<br>採用</span></a></li>
+    <li><a href="<?php echo APP_URL; ?>mid-career/?category=backoffice" class="opacity"><span>バックオフィス<br>採用</span></a></li>
     <li><a href="https://lallyou.afloat.co.jp/recruit/" target="_blank" rel="noopener noreferrer" class="opacity"><span>LallYou<br>採用</span></a></li>
 </ul>
